71d8057ffa97047c3cbeff194f5d7c742edb8cd7
gugucode <meiyuechang@gmail.com>
2018-07-06 18:09:23 -0500

    work version

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+data = [
+    {
+        "name":"Ahmed",
+        "photo":"https://wallpapers.pub/web/wallpapers/birds-water-spray-wallpaper/3840x2160.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name":"Erin",
+        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ226nFD8sG1T_Q-7VLdjHYkpzEzcMDFRXYVRmCcTNm-Eo2sg_",
+        "scores":[
+            5,
+            2,
+            1,
+            2,
+            1,
+            2,
+            4,
+            3,
+            2,
+            4
+        ]
+    },
+    {
+        "name":"GUgu",
+        "photo":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjHUUVEnegkL7EIWKP2bYzLxtXq09aQB_ikBgztZFxjh1aZr8S",
+        "scores":[
+            2,
+            4,
+            2,
+            1,
+            5,
+            3,
+            4,
+            2,
+            4,
+            1
+        ]
+    },
+]
+
+module.exports = data;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,41 @@
 var path = require("path");
+var friends = require(path.join(__dirname,"../data","friends.js"))
+
+function findClosestFriend(my_data){
+    var min_diff = Infinity;
+    var closest_friend;
+    friends.forEach(element => {
+        var friend_score = element.scores;
+        var my_score = my_data.scores;
+
+        var diff = 0;
+        for(var i = 0; i < element.scores.length; i++){
+            diff += Math.abs(friend_score[i] - my_score[i]);
+        }
+        console.log("diff=",diff)
+        if(min_diff > diff){
+            min_diff = diff;
+            closest_friend = element;
+        }
+    });
+
+    return closest_friend;
+}
 
 module.exports = function(app){
     app.get("/api/friends",function(req,res){
-        // res.sendFile(path.join(__dirname,"survey.html"));
+        res.setHeader('Content-Type', 'text/plain')
+        res.end(JSON.stringify(friends));
     })
 
     app.post("/api/friends",function(req,res){
-        // res.sendFile(path.join(__dirname,"home.html"));
+        var new_friend = req.body;
+
+        console.log(new_friend);
+        var closest_friend = findClosestFriend(new_friend);
+        friends.push(new_friend);
+        res.send(closest_friend)
+        
     })
 }
 
